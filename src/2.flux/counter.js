372548882df3083962a249678df9fe6264ea01a0
Xaber <xaber@foxmail.com>
2019-09-20 16:27:06 +0800

    feat: add constructor Counter component

--- src/2.flux/counter.js
+++ src/2.flux/counter.js
@@ -29,4 +29,25 @@ const Counter = (props) => {
 }
 
 // need set withProps true, so that can combile props
-export default Container.createFunctional(Counter, getStores, getState, { withProps: true })
+// export default Container.createFunctional(Counter, getStores, getState, { withProps: true })
+
+
+// 不能用 class 写法，会报错
+function Counter2 () {
+  React.Component.call(this)
+  this.render = function () {
+    // 注意 这里是 state
+    const { decreaseCount, increaseCount, nums, caption } = this.state
+    return <li>
+      <button onClick={() => decreaseCount(caption)}>-</button>
+      <button onClick={() => increaseCount(caption)}>+</button>
+      {caption} Count: {nums[caption]}
+    </li>
+  }
+}
+
+Counter2.prototype = Object.create(React.Component.prototype)
+Counter2.getStores = getStores
+Counter2.calculateState = getState
+
+export default Container.create(Counter2, { withProps: true })
